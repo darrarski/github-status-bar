7d8fa9c62440fdc95b299b035b326f4ab57cca46
Dariusz Rybicki <darrarski@gmail.com>
2020-06-16 17:58:05 +0200

    Refactor side effects in StatusBar.Reducer

--- Sources/StatusBar/Reducer.swift
+++ Sources/StatusBar/Reducer.swift
@@ -7,14 +7,16 @@ public typealias Reducer = ComposableArchitecture.Reducer<State, Action, Environ
 public let reducer = Reducer { state, action, env in
     switch action {
     case .openNotification(let id):
-        if let url = state.notifications.first(where: { $0.id == id })?.webURL {
-            env.urlOpener(url)
+        return .fireAndForget { [state] in
+            if let url = state.notifications.first(where: { $0.id == id })?.webURL {
+                env.urlOpener(url)
+            }
         }
-        return .none
     case .refresh:
         return .none
     case .quit:
-        env.appTerminator(nil)
-        return .none
+        return .fireAndForget {
+            env.appTerminator(nil)
+        }
     }
 }
